892691372e38d44fe3f5bdd7d02e624ac337c1f4
Gregory Ganley <gganley@student.bridgew.edu>
2019-03-01 13:17:02 -0500

    Init

---
+++ config.el
@@ -0,0 +1,8 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq mac-command-modifier 'meta
+      ns-alternate-modifier 'super
+      mac-control-modifier 'control
+      ns-function-modifier 'hyper)
