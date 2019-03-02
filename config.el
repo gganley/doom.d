c15717d0021a6fe0b3446bb82957760e16b3f5a0
Gregory Ganley <me@gganley.com>
2019-03-02 14:04:57 -0500

    Set font to "Fira Mono"

--- config.el
+++ config.el
@@ -6,4 +6,5 @@
       ns-alternate-modifier 'super
       mac-control-modifier 'control
       ns-function-modifier 'hyper)
+(setq doom-font (font-spec :family "Fira Mono" :size 12))
 (setq-default evil-escape-key-sequence "fd")
