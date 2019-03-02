8022fb384f53cb89020dc8ab1c4cb74021383ccc
Gregory Ganley <me@gganley.com>
2019-03-02 14:05:15 -0500

    What does this even do?

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       window-select     ; visually switch windows
+       (window-select +switch-window)     ; visually switch windows
 
        :editor
        fold              ; (nigh) universal code folding
